665a10381686b10ddd4020e67425070c49d3ceca
takashi-07 <ibn5100t@gmail.com>
2023-05-19 19:54:38 +0900

    クイズページ作

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to:"tops#index"
-  get 'calculate', to: 'tops#calculate'
+  resources :questions, only: [:index, :show, :new, :create, :edit, :update]
+
+  
 end
